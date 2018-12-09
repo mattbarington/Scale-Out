55dd3c8007ead6a257e4f38810fcccfb7b111dfa
Ryan Blelloch <rblelloc@ucsc.edu>
2018-12-08 18:45:58 -0800

    making hashing test case

--- hw4_test.py
+++ hw4_test.py
@@ -545,6 +545,44 @@ class TestHW4(unittest.TestCase):
         newShardIDs = self.checkGetAllShardIds(ipPort)
         self.assertEqual(len(newShardIDs), len(initialShardIDs))
 
+    def test_x3_add_keys_test_hash(self):
+
+        print("TEST X3: ADD KEYS TEST HASH")
+        
+        ipPort = self.view[0]["testScriptAddress"]
+        initialShardIDs = self.checkGetAllShardIds(ipPort)
+        
+        all_vals = ["joey", "matt", "parker", "ryan", "peter", \
+                "hpg", "ocelot", "america", "film", "echo", "ashton", \
+                "jeep", "stick", "mind", "boi", "test"]
+
+        all_key_vals = {}
+        local_shard_count = [0,0,0]
+
+        for key in all_vals:
+            thisShard = hash(key) % 3
+            local_shard_count[thisShard] = local_shard_count[thisShard] + 1
+            all_key_vals[key] = str(thisShard)
+
+        for k in all_key_vals:
+
+            print("key: %s , val: %s", k, all_key_vals[k])
+ 
+            payload = self.getPayload(ipPort, k)
+
+            payload = self.confirmAddKey(ipPort=ipPort,
+                           key=k,
+                           value=all_key_vals[k],
+                           expectedStatus=200,
+                           expectedMsg="Added successfully",
+                           expectedReplaced=False,
+                           payload= payload)
+
+        print("waiting for 3 seconds...")
+        time.sleep(propogationTime)
+
+
+
 
     def test_z_add_key_value_one_node(self):
 
